7df8ddccaa2f10613e5aac1b3ac935ac58d0808b
cassiusdc <cassiusdelacruz@gmail.com>
2023-09-26 14:48:00 +0800

    Added clerk auth

---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,12 @@
+import { UserButton } from "@clerk/nextjs";
+
+export default function Home() {
+  return (
+    <div>
+      This is a protected route
+      <div>
+        <UserButton afterSignOutUrl="/"/>
+      </div>
+    </div>
+  )
+}
