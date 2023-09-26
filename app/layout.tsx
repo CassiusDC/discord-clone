b2dd3dd7daa32687c2282e4e526e1d733a4174dc
cassiusdc <cassiusdelacruz@gmail.com>
2023-09-26 17:48:40 +0800

    Modal Provider and main server creation

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,10 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Open_Sans } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
-import { ThemeProvider } from "@/components/providers/theme-provider"
+
 import { cn } from '@/lib/utils'
+import { ThemeProvider } from "@/components/providers/theme-provider"
+import { ModalProvider } from '@/components/providers/modal-provider'
 
 const font = Open_Sans({ subsets: ['latin'] })
 
@@ -20,14 +22,20 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="dark"
-          enableSystem={false}
-          storageKey="discord-theme"
-        >
-          <body className={cn(font.className, "bg-white dark:bg-[#313338]")}>{children}</body>
-        </ThemeProvider>
+      <body className={cn(
+        font.className, 
+        "bg-white dark:bg-[#313338]"
+        )}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem={false}
+            storageKey="discord-theme"
+          >
+            <ModalProvider/>
+            {children}
+          </ThemeProvider>
+        </body>
       </html>
     </ClerkProvider>
   )
